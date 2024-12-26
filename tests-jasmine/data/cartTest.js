1770460639495be69ab9f0e2bf83482443714a80
Tionni <johorali2544@gmail.com>
2024-12-26 22:05:26 +0300

    created a test suite for removefromCart

--- tests-jasmine/data/cartTest.js
+++ tests-jasmine/data/cartTest.js
@@ -1,4 +1,4 @@
-import { addToCart, loadFromStorage, cart } from "../../data/cart.js";
+import { addToCart, loadFromStorage, cart, removeFromCart } from "../../data/cart.js";
 
 describe("test suite: AddToCart", ()=>{
     beforeEach(()=>{
@@ -62,5 +62,47 @@ describe("test suite: AddToCart", ()=>{
 })
 
 describe("test suite: removeFromCart", ()=>{
-    beforeEach(())
+
+    const productId1 = 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6'
+    const productId2 = '15b6fc6f-327a-4ec4-896f-486349e85a3d'
+   
+    beforeEach(()=>{
+        spyOn(localStorage, 'setItem')
+        spyOn(localStorage, 'getItem').and.callFake(()=>{
+            return JSON.stringify([
+                {
+                  productId: productId1,
+                  quantity: 1,
+                  deliveryOptionId: '1'
+  
+                } 
+              ])
+              
+        })
+        loadFromStorage()
+
+    })
+
+    it('remove a productId that is in the cart', ()=>{
+        removeFromCart(productId1)
+        expect(cart.length).toEqual(0)
+        
+        expect(localStorage.setItem).toHaveBeenCalledTimes(1)
+        expect(localStorage.setItem).toHaveBeenCalledWith('cart', JSON.stringify([]))
+        
+    })
+    it('remove a productId that is not in the cart', ()=>{
+        removeFromCart(productId2)
+        expect(cart.length).toEqual(1)
+        expect(localStorage.setItem).toHaveBeenCalledTimes(1)
+        expect(localStorage.setItem).toHaveBeenCalledWith('cart', JSON.stringify([
+            {
+              productId: productId1,
+              quantity: 1,
+              deliveryOptionId: '1'
+
+            } 
+          ]))
+    })
+
 })
