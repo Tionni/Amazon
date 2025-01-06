d2cfd1ad3a2224128997c5b0442a8b0739ed5921
Tionni <johorali2544@gmail.com>
2025-01-06 09:25:38 +0300

    made the search bar interactive and search case case insensitive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -11,7 +11,18 @@ function renderProductsGrid(){
 
     let productsHTML = ""
 
-    products.forEach((product)=>{
+    const url = new URL(window.location.href)
+    const search = url.searchParams.get('search')
+    let filteredProducts = products
+
+    //if a search exists in the URL parameters
+    //filter the products that match the search
+    if(search){
+        filteredProducts = products.filter((product)=>{
+            return product.name.toLowerCase().includes(search)
+        })
+    }
+    filteredProducts.forEach((product)=>{
     productsHTML += `
             <div class="product-container">
             <div class="product-image-container">
@@ -104,4 +115,9 @@ function renderProductsGrid(){
 
         })
     })
+    document.querySelector('.js-search-button').addEventListener('click', ()=> {
+        const search = document.querySelector('.js-search-bar').value 
+        window.location.href = `amazon.html?search=${search}`
+    })
+
 }
